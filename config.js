4ab942f1b868d1a62f5d3c88cecac61ebb604bc7
Mgdo1 <133541870+Mgdo1@users.noreply.github.com>
2023-05-14 20:42:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,12 +21,12 @@ global.lApiKey = [ // if this API key get expired then please go to https://api.
 ]
 
 
-global.botname = "Queen Anya" 
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
+global.botname = "KING MGDAD" 
+global.author = "MGDAD"  
+global.packname = "KING✯MGDAD✯𝘽𝙊𝙏"  
 global.myweb = "https://github.com/PikaBotz" // You can put any link 
-global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.footer = "© Queen Anya Bot"
+global.waterMark = "×͜× King ✘ MGDAD™ ✓" // Your own watermark 
+global.footer = "©  MGDAD Bot"
 global.prefa = ['-'] // single prefix
 global.themeemoji = '🔥'
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
@@ -34,14 +34,14 @@ global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM �
 
 
 global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['918811074852'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
-global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
+global.ownernumber = ['249962204268'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : NO " // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ytname = "YT: professional tech "   // Yt chanel name //
+global.continent = "sd" // your continent name 
+global.region = "sudan" // your country name
+global.state = "madani" // your state name
 global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
+global.timezone = "sudan" // search on google if you don't know the timezone of your country //
 global.instagramId = "3.69_pika" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
@@ -54,8 +54,8 @@ global.greet4 = "Good night 😴" // after 8:30 PM
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','91881107****']  // Co - Owners
+global.premium = ['249962204268','249962204268']  // available soon
+global.sudo = ['249962204268','249962204268']  // Co - Owners
 
 
 //--------------- Tip ----------------\\
